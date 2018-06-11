e6be109409bda5afe353cfef8af004b50412dddd
entradajuan <entradajuan@yahoo.es>
2018-06-11 19:50:52 +0200

    beginning

--- producer01/src/main/java/com/needine/producer01/MainBean.java
+++ producer01/src/main/java/com/needine/producer01/MainBean.java
@@ -1,19 +1,18 @@
 package com.needine.producer01;
 
-import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.Serializable;
-import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
 
 @Named
 @ViewScoped
@@ -23,11 +22,7 @@ public class MainBean implements Serializable {
 	
 	private String userId;
 	private String msgId;
-	
-	private String targetUrl;
-	private Client client;
-    private Response response;
-	
+		
 	public String getUserId() {
 		return userId;
 	}
@@ -44,36 +39,32 @@ public class MainBean implements Serializable {
 		this.msgId = msgId;
 	}
 
-	private static URI getBaseURI() {
-        return UriBuilder.fromUri("http://localhost:9080/consumer01").build();
-    }
-	  
 	public void save() {
-		System.out.println("user " + getUserId() + " likes " + getMsgId());
+		String string = "{\"user\": \"" + getUserId() + "\", \"msg\": \"" + getMsgId() + "\"}";
+		JSONObject jsonObject = new JSONObject(string);
 
-		//targetUrl = "http://localhost:9080/consumer01/rest/hello";
-		client = ClientBuilder.newClient();
-		
-		WebTarget target = client.target(getBaseURI());
-    	String response = target.path("rest").
-                path("hello").
-                request().
-                accept(MediaType.TEXT_PLAIN).
-                get(Response.class)
-                .toString();
+		// Step2: Now pass JSON File Data to REST Service
+		try {
+			URL url = new URL("http://localhost:9080/consumer01/rest/likesService");
+			URLConnection connection = url.openConnection();
+			connection.setDoOutput(true);
+			connection.setRequestProperty("Content-Type", "application/json");
+			connection.setConnectTimeout(5000);
+			connection.setReadTimeout(5000);
+			OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+			out.write(jsonObject.toString());
+			out.close();
 
-		String plainAnswer =
-		    target.path("rest").path("hello").request().accept(MediaType.TEXT_PLAIN).get(String.class);
-		String xmlAnswer =
-		    target.path("rest").path("hello").request().accept(MediaType.TEXT_XML).get(String.class);
-		String htmlAnswer=
-		    target.path("rest").path("hello").request().accept(MediaType.TEXT_HTML).get(String.class);
-		
-		System.out.println(response);
-		System.out.println(plainAnswer);
-		System.out.println(xmlAnswer);
-		System.out.println(htmlAnswer);
-		
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+			while (in.readLine() != null) {
+			}
+			System.out.println("\nCrunchify REST Service Invoked Successfully..");
+			in.close();
+		} catch (Exception e) {
+			System.out.println("\nError while calling Crunchify REST Service");
+			System.out.println(e);
+		}
 		
 		FacesMessage message = new FacesMessage("DONE!! " + getUserId());
 		message.setSeverity(FacesMessage.SEVERITY_INFO);
